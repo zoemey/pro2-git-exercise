1093cd358df908e0a1dcf5fe1e3fbfc75c690b61
zoemey <121060018+zoemey@users.noreply.github.com>
2026-02-19 12:26:17 +0100

    drei schlechteste noten

--- noten.py
+++ noten.py
@@ -8,3 +8,22 @@ durchschnitt = (mathe+englisch+programmieren+statistik)/4
 noten = {
     "mathe": 5, "englisch": 4, "programmieren": 2, "statistik": 6
 }
+
+
+
+summe = 0
+
+for i in range(3):
+    schlechtestes_fach = ""
+    schlechteste_note = 6
+
+    for fach in noten:
+        if noten[fach] < schlechteste_note:
+            schlechteste_note = noten[fach]
+            schlechtestes_fach = fach
+
+    summe += schlechteste_note
+    del noten[schlechtestes_fach]
+
+durchschnitt = summe / 3
+print("Durchschnitt der 3 schlechtesten:", durchschnitt)
