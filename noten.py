e507521c1fb163ea3a912237b00e49b3755a578a
AlinaSteiger <alina.steiger@stud.fhgr.ch>
2026-02-19 12:23:10 +0100

    Besten drei Noten ausgelesen

--- noten.py
+++ noten.py
@@ -8,3 +8,15 @@ durchschnitt = (mathe+englisch+programmieren+statistik)/4
 noten = {
     "mathe": 5, "englisch": 4, "programmieren": 2, "statistik": 6
 }
+
+for i in range(3):
+    bestes_fach = ""
+    beste_note = 0
+
+    for fach in noten:
+        if noten[fach] > beste_note:
+            beste_note = noten[fach]
+            bestes_fach = fach
+
+    print(bestes_fach, beste_note)
+    del noten[bestes_fach]
